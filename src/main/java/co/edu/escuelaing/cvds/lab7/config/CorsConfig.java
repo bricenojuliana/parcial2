f94ddd89ad246fa918c183d5a1314767fe59e8c3
Juliana Briceño <bricenojuliana@gmail.com>
2024-04-12 16:43:00 -0500

    cors config

--- src/main/java/co/edu/escuelaing/cvds/lab7/config/CorsConfig.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }
